716c32adf3ef378148e6c1218facecce9d4724d9
Nik0-9 <filannino.nicola9@gmail.com>
2024-03-05 15:55:18 +0100

    add method output

--- js/myscript.js
+++ js/myscript.js
@@ -3,13 +3,18 @@ poi chiedi il suo cognome,
 poi chiedi il suo colore preferito
 Infine scrivi sulla pagina nomecognomecolorepreferito23*/
 
-let userName, userLastName, favouriteColor;
+let userName, userLastName, favouriteColor,message;
 userName = prompt('Scrivi il tuo nome:');
 userLastName = prompt('Scrivi il tuo cognome:');
 favouriteColor = prompt('Scrivi il tuo colore preferito:');
 console.log(userName, userLastName, favouriteColor);
+message = userName + userLastName + favouriteColor + '23'
 
 
 document.getElementById('pswd').innerHTML = userName + userLastName + favouriteColor + '23';
 
 document.getElementById('pswd2').innerHTML = `${userName}${userLastName}${favouriteColor}23`
+
+document.write(message);
+
+
